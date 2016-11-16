d4594ea983efe341f3377cd70b0d7931ecf87339
jay <jay.chang@redso.com.hk>
2016-11-17 01:36:48 +0800

    Fix wrong pressed background and text color

--- library/src/main/java/com/jaychang/st/RoundedBackgroundSpan.java
+++ library/src/main/java/com/jaychang/st/RoundedBackgroundSpan.java
@@ -9,6 +9,7 @@ import android.text.TextPaint;
 import android.text.style.ReplacementSpan;
 
 import static android.R.attr.padding;
+import static android.R.attr.width;
 
 public class RoundedBackgroundSpan extends ReplacementSpan {
 
@@ -16,9 +17,9 @@ public class RoundedBackgroundSpan extends ReplacementSpan {
   private int backgroundColor;
   private int textColor;
 
-  public RoundedBackgroundSpan(@ColorInt int backgroundColor,
-                                @ColorInt int textColor,
-                                int radius) {
+  public RoundedBackgroundSpan(@ColorInt int textColor,
+                               @ColorInt int backgroundColor,
+                               int radius) {
     super();
     this.backgroundColor = backgroundColor;
     this.textColor = textColor;
@@ -40,23 +41,27 @@ public class RoundedBackgroundSpan extends ReplacementSpan {
   @Override
   public void draw(@NonNull Canvas canvas, CharSequence text,
                    int start, int end, float x, int top, int y, int bottom, @NonNull Paint paint) {
-    float width = paint.measureText(text.subSequence(start, end).toString());
-    int originalTextColor = paint.getColor();
-
-    int extra1Dp = (int)(((TextPaint) paint).density * 1 + 0.5f);
-    int newTop = (int) (bottom-paint.getFontSpacing()-paint.descent())+2*extra1Dp;
-    int newBottom = bottom - extra1Dp;
     boolean isLeftEdge = x == 0.0f;
-    int newLeft = (int) (isLeftEdge ? x : x - radius);
-    int newRight = (int) (isLeftEdge ? x + width + 2 * radius : x + width + radius);
-    RectF rect = new RectF(newLeft, newTop, newRight, newBottom);
-    paint.setColor(backgroundColor);
-    canvas.drawRoundRect(rect, radius, radius, paint);
 
-    float textX = isLeftEdge ? x + radius : x;
-    float textY = bottom - paint.getFontMetricsInt().bottom;
-    paint.setColor(textColor == 0 ?  originalTextColor: textColor);
-    canvas.drawText(text, start, end, textX, textY, paint);
+    if (backgroundColor != 0) {
+      int extra1Dp = (int) (((TextPaint) paint).density * 1 + 0.5f);
+      float width = paint.measureText(text.subSequence(start, end).toString());
+      int newTop = (int) (bottom - paint.getFontSpacing() - paint.descent()) + 2 * extra1Dp;
+      int newBottom = bottom - extra1Dp;
+      int newLeft = (int) (isLeftEdge ? x : x - radius);
+      int newRight = (int) (isLeftEdge ? x + width + 2 * radius : x + width + radius);
+      RectF rect = new RectF(newLeft, newTop, newRight, newBottom);
+      paint.setColor(backgroundColor);
+      canvas.drawRoundRect(rect, radius, radius, paint);
+    }
+
+    if (textColor != 0) {
+      int originalTextColor = paint.getColor();
+      float textX = isLeftEdge ? x + radius : x;
+      float textY = backgroundColor == 0 ? bottom - paint.descent() : bottom - paint.getFontMetricsInt().bottom;
+      paint.setColor(textColor == 0 ? originalTextColor : textColor);
+      canvas.drawText(text, start, end, textX, textY, paint);
+    }
   }
 
 }
